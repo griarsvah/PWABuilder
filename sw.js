96ffd3f836054d725434844f6b91cb7ca853aa12
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-10 15:16:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,7 +57,7 @@ self.addEventListener('push', (event) => {
   event.waitUntil(
     self.registration.showNotification('Notification Title', {
       body: 'Notification Body Text',
-      icon: 'custom-notification-icon.png',
+      icon: 'images/vector.svg',
     });
   );
 });
