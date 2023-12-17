16b6f75ff4de35836ef18ad8c28ed531c727214a
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-17 10:42:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,14 +11,61 @@ const PRECACHE_ASSETS = [
 
 const offlineFallbackPage = "index.html";
 
-
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        cache.addAll(PRECACHE_ASSETS);
-        caches.open(CACHE)
-        .then((cache) => cache.add(offlineFallbackPage))
-    })());
+self.addEventListener('install', async (event) => {
+  event.waitUntil(      
+    cache.addAll(PRECACHE_ASSETS);
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
 });
+
+
+
+
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+
+
+
+
 
 self.addEventListener('notificationclick', (event) => {
     event.notification.close(); 
