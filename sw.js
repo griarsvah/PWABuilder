05d0e892c08ea5c9c5e5350a9f1e57f8cb5412dd
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-17 10:04:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,9 +6,17 @@ const PRECACHE_ASSETS = [
     '/images/'
 ]
 
+
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE);
         cache.addAll(PRECACHE_ASSETS);
     })());
+});
+
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close(); 
+    var fullPath = self.location.origin + event.notification.data.path; 
+    clients.openWindow(fullPath); 
 });
