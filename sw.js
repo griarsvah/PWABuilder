2eddaa229c8c4073e8b4daa12782a982169ead60
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-17 09:11:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,74 +2,13 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.6.1/workbox
 
 const CACHE_NAME = 'cool-cache';
 
-// Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
     '/images/'
 ]
-
-contentToCache.push("images/vector.svg");
 
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE);
         cache.addAll(PRECACHE_ASSETS);
     })());
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
-
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
-
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
-});
-
-self.addEventListener('push', (event) => {
-  event.waitUntil(
-    self.registration.showNotification('Notification Title', {
-      body: 'Notification Body Text',
-      icon: 'images/vector.svg'
-    });
-  );
-});
-
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close(); 
-    var fullPath = self.location.origin + event.notification.data.path; 
-    clients.openWindow(fullPath); 
-});
-
-// Add an event listener for the `sync` event in your service worker.
-self.addEventListener('sync', event => {
-
-  // Check for correct tag on the sync event.
-  if (event.tag === 'database-sync') {
-
-    // Execute the desired behavior with waitUntil().
-    event.waitUntil(
-
-      // This is just a hypothetical function for the behavior we desire.
-      pushLocalDataToDatabase();
-    );
-    }
-});
-
-self.addEventListener('periodicsync', (event) => {
-});
-
-self.addEventListener('message', (event) => {
 });
