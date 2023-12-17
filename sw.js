4c093038eca4b4d94571fa0acaf4fcd5ecaa2df0
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-17 11:42:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -66,8 +66,43 @@ self.addEventListener("message", (event) => {
 
 
 
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close(); 
-    var fullPath = self.location.origin + event.notification.data.path; 
-    clients.openWindow(fullPath); 
+
+
+
+self.addEventListener('push', function (event) {
+    if (Notification.permission === "granted") {
+        let notification_data = JSON.parse(event.data.text());
+        const notificationText = notification_data["description"];
+        const notification_title = notification_data["notifier_DisplayName"];
+        let notificaiotn_timestampt = notification_data["CreatedAt"];
+        let notification_timestamp = new Date(notificaiotn_timestampt * 1000);
+        let notification_timestamp_string = notification_timestamp.toLocaleString();
+        let notificaiton_sid = notification_data["sid"];
+
+
+        const showNotification = self.registration.showNotification(notification_title, {
+            // title: notification_title,
+            body: notificationText,
+            icon: '/images/vector.svg',
+            tag: notificaiton_sid,
+            data: {
+                url: notification_data["url"]
+            },
+            // actions: [
+            //     { action: 'actionName', title: 'Mark as read', icon: '/images/vector.svg' },
+            //     { action: 'actionName', title: 'Mark as unread', icon: '/images/vector.svg' },
+            // ],
+            timestamp: notification_timestamp_string,
+        });
+        event.waitUntil(showNotification);
+    }
+});
+
+
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
 });
