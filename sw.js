130a43abba0f0b747541be6a6c84dc93d4ae34d3
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-19 22:17:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -69,6 +69,50 @@ self.addEventListener("message", (event) => {
 
 
 
+
+
+
+
+
+
+
+// Query the user for permission.
+const periodicSyncPermission = await navigator.permissions.query({
+  name: 'periodic-background-sync',
+});
+// Check if permission was properly granted.
+if (periodicSyncPermission.state == 'granted') {
+
+  // Register a new periodic sync.
+  await registration.periodicSync.register('fetch-new-content', {
+    // Set the sync to happen no more than once a day.
+    minInterval: 24 * 60 * 60 * 1000
+  });
+}
+// Listen for the `periodicsync` event.
+self.addEventListener('periodicsync', event => {
+
+  // Check for correct tag on the periodicSyncPermissionsync event.
+  if (event.tag === 'fetch-new-content') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      fetchNewContent();
+    );
+  }
+});
+
+
+
+
+
+
+
+
+
+
 self.addEventListener('push', function (event) {
     if (Notification.permission === "granted") {
         let notification_data = JSON.parse(event.data.text());
