5f74662af08afa8e30f8c0df98fe755f7666948e
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-23 14:25:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,11 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.6.1/workbox-sw.js");
-
-
-
-
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
 const CACHE = "pwabuilder-offline-page";
 
-const offlineFallbackPage = "index.html";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-
-
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -17,24 +13,12 @@ self.addEventListener("message", (event) => {
   }
 });
 
-
-
-
-
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
       .then((cache) => cache.add(offlineFallbackPage))
   );
 });
-
-
-
-
-
-
-
-
 
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
@@ -67,94 +51,4 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Query the user for permission.
-const periodicSyncPermission =  navigator.permissions.query({
-  name: 'periodic-background-sync',
-});
-// Check if permission was properly granted.
-if (periodicSyncPermission.state == 'granted') {
-
-  // Register a new periodic sync.
-   registration.periodicSync.register('fetch-new-content', {
-    // Set the sync to happen no more than once a day.
-    minInterval: 24 * 60 * 60 * 1000
-  });
-}
-// Listen for the `periodicsync` event.
-self.addEventListener('periodicsync', event => {
-
-  // Check for correct tag on the periodicSyncPermissionsync event.
-  if (event.tag === 'fetch-new-content') {
-
-    // Execute the desired behavior with waitUntil().
-    event.waitUntil(
-
-      // This is just a hypothetical function for the behavior we desire.
-      fetchNewContent();
-    );
-  };
-});
-
-
-
-
-
-
-
-
-
-
-self.addEventListener('push', function (event) {
-    if (Notification.permission === "granted") {
-        let notification_data = JSON.parse(event.data.text());
-        const notificationText = notification_data["description"];
-        const notification_title = notification_data["notifier_DisplayName"];
-        let notificaiotn_timestampt = notification_data["CreatedAt"];
-        let notification_timestamp = new Date(notificaiotn_timestampt * 1000);
-        let notification_timestamp_string = notification_timestamp.toLocaleString();
-        let notificaiton_sid = notification_data["sid"];
-
-
-        const showNotification = self.registration.showNotification(notification_title, {
-            // title: notification_title,
-            body: notificationText,
-            icon: '/images/vector.svg',
-            tag: notificaiton_sid,
-            data: {
-                url: notification_data["url"]
-            },
-            // actions: [
-            //     { action: 'actionName', title: 'Mark as read', icon: '/images/vector.svg' },
-            //     { action: 'actionName', title: 'Mark as unread', icon: '/images/vector.svg' },
-            // ],
-            timestamp: notification_timestamp_string,
-        });
-        event.waitUntil(showNotification);
-    }
-});
-
-
-
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data.url)
-    );
 });
