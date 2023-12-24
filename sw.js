625c452ac73757e61af0de1543f7d91de1021d0c
Vahe <148644489+griarsvah@users.noreply.github.com>
2023-12-24 08:10:09 +0300

    push

--- sw.js
+++ sw.js
@@ -50,3 +50,58 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Respond to a server push with a user notification.
+self.addEventListener('push', function (event) {
+    if (Notification.permission === "granted") {
+        let notification_data = JSON.parse(event.data.text());
+        const notificationText = notification_data["description"];
+        const notification_title = notification_data["notifier_DisplayName"];
+        let notificaiotn_timestampt = notification_data["CreatedAt"];
+        // Convert the string date to a date object. And unix timestamp to milliseconds
+        let notification_timestamp = new Date(notificaiotn_timestampt * 1000);
+        // Convert the date object to a string, using the locale language
+        let notification_timestamp_string = notification_timestamp.toLocaleString();
+        let notificaiton_sid = notification_data["sid"];
+
+
+        const showNotification = self.registration.showNotification(notification_title, {
+            // title: notification_title,
+            body: notificationText,
+            icon: '/images/vector.svg',
+            tag: notificaiton_sid,
+            data: {
+                url: notification_data["url"]
+            },
+            // actions: [
+            //     { action: 'actionName', title: 'Mark as read', icon: '/static/images/Logos/logo.png' },
+            //     { action: 'actionName', title: 'Mark as unread', icon: '/static/images/Logos/logo.png' },
+            // ],
+            timestamp: notification_timestamp_string,
+        });
+        // Make sure the toast notification is displayed.
+        event.waitUntil(showNotification);
+    }
+});
+
+// Respond to the user selecting the toast notification.
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url) // This line will open the URL saved in 'data' when the notification was first created.
+    );
+});
